87db4db3718b5bedf03a88cb0d650bf381009977
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-09-28 23:47:24 -0400

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,27 @@
+public class Date{
+  public static void main(String[] args){
+    System.out.println("American Format: ");
+    String day = "Wednesday";
+    String month = "September";
+    int date = 1;
+    int year = 2021;
+    System.out.print(day);
+    System.out.print(",");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(", ");
+    System.out.print(year);
+    System.out.println(" ");
+
+    System.out.println("European Format: ");
+    System.out.print(day);
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(" ");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.print(year);
+    System.out.println(" ");
+  }
+}
