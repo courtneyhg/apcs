0014f97767f04cc7aaeebe9b5309d7d1cfbaec04
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-09-30 15:18:30 -0400

    Update Greet.java

--- 07_helloclass/Greet.java
+++ 07_helloclass/Greet.java
@@ -1,5 +1,5 @@
-public class Greet{
-  public static void main (String [] args){
-    System.out.println("No hablo queso");
-  }
+public class Greet {
+   public static void main(String[] args) {
+      System.out.println("No hablo queso");
+   } 
 }
