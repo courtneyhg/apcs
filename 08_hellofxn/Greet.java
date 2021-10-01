a152d3b2277643f1a32632c8f8b20b8d77514a9b
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-01 01:42:40 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,14 @@
+// <Courtney> <Huang>
+// APCS
+// HW<08> -- <Refactor Freshie Zero>
+// <2021>-<09>-<30>
+
+// DISCOVERIES
+// You must include (String str) because you greet is displaying a string as the outcome.
+
+// UNRESOLVED QUESTIONS
+// No unresolved questions so far
+
 public class Greet {
    public static void main(String[] args) {
       greet("Hello Courtney!");
