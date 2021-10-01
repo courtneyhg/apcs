e93990b3d4b193f508652ecfa2bb9b4450f65964
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-01 01:23:35 -0400

    Update Arrays.java

--- pre/08/Arrays.java
+++ pre/08/Arrays.java
@@ -24,3 +24,20 @@ public class Chapter8exercise1{
   }
 
 }
+
+public class indexOfMax{
+  public static void main(String[] args){
+    int[] array1 = {8, 45, 83, 23, 99};
+    System.out.println(sort(array1));
+
+}
+  public static int sort(int[] array1){
+    int index=0;
+    for (int i=0; i < array1.length; i++) {
+      if (array1[i] > array1[index]){
+        index = i;
+      }
+    }
+    return index;
+  }
+}
