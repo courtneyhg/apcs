509fbd9a8525a0d37625459102440911d98263f9
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-01 00:51:13 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,7 @@
 public class Greet {
    public static void main(String[] args) {
-      System.out.println("No hablo queso");
+      greet("Hello Courtney!");
    } 
+   static void greet(String str){
+      System.out.println(str);
 }
