9676a876adba8512285f1b41c5f5d80d51118e43
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-01 20:11:20 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,13 @@
+// <Flaming> <Hot> <Cheetos> <Duckies Name: Timothy>
+// APCS
+// HW<09> -- <What are BigSibs Good For?>
+// <2021>-<09>-<30>
+
+// DISCOVERIES
+// This is VERY similar to the code from HW_08. We use greet to display the strings.
+
+// UNRESOLVED QUESTIONS
+
 public class BigSib {
   public static void main (String [] args) {
     BigSib.greet("Film");
