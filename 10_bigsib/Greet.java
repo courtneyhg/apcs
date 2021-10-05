ae6ec41380207beb05de9b9d2d7da2201f297fff
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-05 00:32:50 -0400

    Update Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,16 @@
+// <Flaming> <Hot> <Cheetos> <Courtney Huang and Timothy, Perry Huang and Salaj Jr., Samantha Hua and Poppy>
+// APCS
+// HW<10> -- <Refractor Big Sib One>
+// <2021>-<10>-<04>
+
+// DISCOVERIES
+// This is VERY similar to the code from HW_08. We use greet to display the strings.
+
+// UNRESOLVED QUESTIONS
+
+public class Greet {
+  public static void main (String [] args) {
+    System.out.println(BigSib.greet("Crosby"));
+    System.out.println(BigSib.greet("Stills"));
+    System.out.println(BigSib.greet("Nash"));
+}
