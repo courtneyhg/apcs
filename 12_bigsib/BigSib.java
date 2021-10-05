26f2b6c49f35f96e428f0823434d48c3f7959507
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-05 18:56:01 -0400

    Update BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,24 @@
+// <Flaming> <Hot> <Cheetos> <Courtney Huang and Timothy, Perry Huang and Salaj Jr., Samantha Hua and Poppy>
+// APCS
+// HW<12> -- <On Elder Individuality and the Elimination of Radio Fuzz>
+// <2021>-<10>-<05>
+
+// DISCO
+// 
+
+// QCC
+//
+
+public class Greet {
+  public static void main (String [] args) {
+    System.out.println(BigSib.greet("Word up freshman"));
+    System.out.println(BigSib.greet("Salutations Dr. Spaceman"));
+    System.out.println(BigSib.greet("Hey ya Kong Fooey"));
+    System.out.println(BigSib.greet("Sup mom"));
+}
+
+public class BigSib{
+  public static String greet(String x) {
+    return (x);
+   }
+}
