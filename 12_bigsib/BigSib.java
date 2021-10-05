85405d62ca08057f3d051aa882a4825fbece2a71
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-05 19:12:02 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -9,13 +9,8 @@
 // QCC
 //
 
-public class Greet {
-  public static void main (String [] args) {
-    String greeeting;
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
-    
-    greeting = richard.greet("freshman");
-    System.out.println(greeting);
-  }
+ public class BigSib{
+  public static String greet(String x) {
+    return (x);
+   }
 }
