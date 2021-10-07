e7ef3d844b8717b8aaff7b245ab0e20c436a445a
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-07 00:57:09 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -14,7 +14,7 @@ public class Greet {
     String greeeting;
     
     BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+    //richard.setHelloMsg("Word up");
     
     greeting = richard.greet("freshman");
     System.out.println(greeting);
