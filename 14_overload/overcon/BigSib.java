a63dedabe39f1133388f3de98567a4def6aa9e34
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-08 04:06:04 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,3 +1,14 @@
+// <Courtney Huang and Timothy, Yat Long>
+// APCS
+// HW<12> -- <Customize Your Creation>
+// <2021>-<10>-<07>
+
+// DISCO
+// I gave HelloMsg a value so when greet is run it returns the HelloMsg and String x.
+
+// QCC
+// I keep getting the error message "Error: Main method not found in class BigSib, please define the main method as: public static void main(String[] args)"
+
 public class BigSib{
   String HelloMsg;
   public BigSib () {
