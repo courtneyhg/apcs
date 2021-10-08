ea420fe77f78d5e259502d9a0ed98ba2c4f9a5f8
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-08 03:41:43 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -1,1 +1,24 @@
+// <Courtney Huang and Timothy, Yat Long>
+// APCS
+// HW<12> -- <Customize Your Creation>
+// <2021>-<10>-<07>
 
+// DISCO
+// A constructor must be public and it is the 1st method definition
+
+// QCC
+// I keep getting the error message "error: cannot find symbol" "symbol: variable greeting" "location: class Greet"
+
+ public class BigSib{
+  String HelloMsg;
+  public BigSib () {
+   HelloMsg = ("Word up");
+  }
+  
+  public void setHelloMsg(String y) {
+   HelloMsg = y;
+  }
+  public String greet(String x) {
+   return (HelloMsg + " " + x);
+   }
+}
