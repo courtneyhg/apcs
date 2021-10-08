459b99b57d4e97ca9b0261124e84fd54a763ffcf
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-08 03:58:46 -0400

    Update BigSib.java

---
+++ 14_overload/overcon/BigSib.java
@@ -0,0 +1,13 @@
+public class BigSib{
+  String HelloMsg;
+  public BigSib () {
+   HelloMsg = ("Word up");
+  }
+  
+  public void setHelloMsg(String y) {
+   HelloMsg = y;
+  }
+  public String greet(String x) {
+   return (HelloMsg + " " + x + "\nSalutations Dr. Spaceman" + "\nHey ya Kong fooey" + "\nSup mom");
+   }
+}
