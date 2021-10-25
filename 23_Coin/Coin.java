1e2eb4136b9549f28e855b882532f4e333e396c8
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-24 20:50:24 -0400

    Update Coin.java

--- 23_Coin/Coin.java
+++ 23_Coin/Coin.java
@@ -1,26 +1,22 @@
-
+/*
+Mochi Yoshi - Yuki Feng, Courtney Huang
+APCS
+HW 23: What Does Equality Look Like?
+2021 10 23
+Time spent: 1.5 hours */
+import java.util.Random;
 public class Coin {
-	public static void main(String[] args) {
-	}
-  //attributes aka instance vars
-	float value;
-	boolean upFace;
-	String name;
-	int flipCtr;
-	int headsCtr;
-	int tailsCtr;
-	float bias;
 
+  // attributes aka instance vars
+  private double value;
+  private String upFace;
+  private String denomination;
+  private int flipCtr, headsCtr, tailsCtr;
+  private double bias;
 
-  /***
-   *  Coin() -- default constuctor
-   *  precond:
-   *  postcond:
-   ***/
+  // default contructor
   public Coin() {
-
   }
-
 
   /***
       Coin(String) -- overloaded constructor
@@ -34,46 +30,51 @@ public class Coin {
       postcond:
   ***/
   public Coin( String s ) {
-	String coinType = name;
-	name = s;
-	return coinType;
-	
+    this.denomination = s;
+    //name = s;
   }
 
 
   /***
-      Coin(String,String) --
+      Coin(String,String) -- 2nd overloaded contructor
       precond:
       postcond:
   ***/
   public Coin( String s, String nowFace ) {
-
-  }
-
+    // this.denomination = s;
+    String oldFace = upFace;
+    upFace = nowFace;
 
   // Accessors...
   // ----------------------------
-  public String getUpFace() {
-
+  /*
+  public String getUpFace(String newFace) {
+    String face = upFace;
+    upFace = newFace;
+    return face;
   }
-
-  public int getFlipCtr() {
-
+  public int getFlipCtr(int flip) {
+    int flipNum = flipCtr;
+    flipCtr = flip;
+    return flipNum;
   }
-
-  public double getValue() {
-
+  public double getValue(int val) {
+    int amount = value;
+    value = val;
+    return amount;
   }
-
-  public int getHeadsCtr() {
-
+  public int getHeadsCtr(int head) {
+    int headNum = headsCtr;
+    headsCtr = head;
+    return headNum;
   }
-
-  public int getTailsCtr() {
-
+  public int getTailsCtr(int tail) {
+    int tailNum = tailsCtr;
+    tailsCtr = tail;
+    return tailNum;
   }
+  */
   // ----------------------------
-
 
   /***
    * assignValue() -- set a Coin's monetary value based on its name
@@ -81,10 +82,28 @@ public class Coin {
    * postcond: instvar value gets appropriate value
    * Returns value assigned.
    ***/
-  private double assignValue( String s ) {
 
+  public double assignValue( String s ) {
+    if(s == "penny"){
+		    value = 0.01;
+		}
+		else if(s == "nickel"){
+		    value = 0.25;
+		}
+		else if(s == "dime"){
+		    value = 0.10;
+		}
+		else if(s == "quarter"){
+		    value = 0.25;
+		}
+		else if(s == "half dollar"){
+		    value = 0.50;
+		}
+		else if (s == "dollar"){
+		    value = 1.00;
+		}
+		return value;
   }
-
 
   /***
       reset() -- initialize a Coin
@@ -92,9 +111,8 @@ public class Coin {
       postcond: Coin's attribs reset to starting vals
   ***/
   public void reset( String s, double d ) {
-
+    upFace = nowFace;
   }
-
 
   /***
    * String flip() -- simulates a Coin flip
@@ -105,11 +123,20 @@ public class Coin {
    * Either headsCtr or tailsCtr incremented by 1, as approp.
    * Returns "heads" or "tails"
    ***/
+  /*
   public String flip() {
-
+    Double[] values = {0.01, 0.05, 0.10, 0.25, 0.50, 1.00};
+    int i = 0;
+    flipCtr = 0;
+    if (value >= 0.0 && value <= 1.0) {
+      value = value.random();
+    }
+    for (i=0; i<values.length(); i ++) {
+      System.out.println(value);
+      flipCtr += 1;
+    }
   }
-
-
+*/
   /***
    * boolean equals(Coin) -- checks to see if 2 coins have same face up
    * precond: other is not null
@@ -117,9 +144,13 @@ public class Coin {
    * or both showing tails. False otherwise.
    ***/
   public boolean equals( Coin other ) {
-
+    // return (other == denomination);
+    if (upFace == other.upFace){
+			return true;
+		} else {
+			return false;
+		}
   }
-
 
   /***
    * String toString() -- overrides toString() provided by Java
@@ -127,7 +158,15 @@ public class Coin {
    * postcond: Return String comprised of name and current face
    ***/
   public String toString() {
+    String reStr = "\nYour coin:\n-------------";
+    reStr += "\nCurrent denomination: " + denomination;
+    reStr += "\nCurrent face: " + upFace;
+    return reStr;
+  }
 
+  public static void main(String[] args){
+    Coin mine = new Coin();
+    // mine.name("penny");
   }
 
 }//end class
