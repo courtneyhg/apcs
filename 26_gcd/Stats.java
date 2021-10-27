b2747d66859257dee81bd2d9744f6cc5a15604ad
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-27 18:51:08 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -6,11 +6,10 @@ public class Stats {
 		System.out.println(gcd(5, 5));
 	}
 	
-	
+	int counter;
+	int answer;
 	// "brute force" approach beginning at 1 and working up to lesser input
 	public static int gcd(int a, int b){
-		int counter;
-		int answer;
 		while (1 < counter){
 			if ((a%counter==0) && (b%counter==0)){
 				answer += 1;
