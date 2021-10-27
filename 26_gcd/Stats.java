a771e85b2d9930319d075dceb05bce22ff3428f8
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-27 19:38:21 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -6,7 +6,7 @@ public class Stats {
 		System.out.println(gcdEW(5, 5));
 	}
 	
-	/*
+	
 	// "brute force" approach beginning at 1 and working up to lesser input
 	public static int gcd(int a, int b){
 		int counter = 1;
@@ -23,7 +23,6 @@ public class Stats {
 			}
 			return answer;
 		}
-	*/
 	
 	// recursive algorithmn
 	//public static int gcdER(int a, int b){
