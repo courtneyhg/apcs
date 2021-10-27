c7f201c407d9e069257988a44f6a82bade737b18
Courtney Huang <chuang31@stuy.edu>
2021-10-27 17:02:49 -0400

    hw

---
+++ 26_gcd/Stats.java
@@ -0,0 +1,40 @@
+public class Stats {
+	public static void main (String [] args){
+		System.out.println(gcd(15, 18));
+		System.out.println(gcd(4, 24));
+		System.out.println(gcd(21, 14));
+		System.out.println(gcd(5, 5));
+	}
+	
+	
+	// "brute force" approach beginning at 1 and working up to lesser input
+	public static int gcd(int a, int b){
+		
+	}
+	
+	// recursive algorithmn
+	//public static int gcdER(int a, int b){
+	//}
+	
+	// while-looped based iterative algotitmn
+	public static int gcdEW(int a, int b){
+		int counter;
+		if (a == b){
+		return a;
+		}
+		else if (a > b){
+		counter = (a-b);
+		}
+		else{
+		counter = (b-a);
+		}
+		
+		while (1 < counter){
+			if ((a%counter==0) && (b%counter==0)){
+			return counter;
+			}
+			counter -= 1;
+			}
+			return counter;
+	}
+}
