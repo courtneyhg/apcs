3ade39796945440a695e6b95400212b612bd7fed
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-10-28 22:37:54 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -4,19 +4,23 @@ public class Rof {
 		System.out.println(fenceF(1));
 		System.out.println(fenceF(2));
 		System.out.println(fenceF(3));
+
+    	System.out.println(reverseF("stressed"));
 		/*
-		int x = 0;
-		while (x < 4) {
-			System.out.println( x);
-			x++;
+		// basic example of a while loop converted into a for loop:
+			int x = 0;
+			while (x < 4) {
+				System.out.println( x);
+				x++;
+			}
+
+			for (int i = 0; i <4; i++) {
+			System.out.println(i);
+			}
+			*/
 		}
-		
-		for (int i = 0; i <4; i++) {
-		System.out.println(i);
-		}
-		*/
-	}
-	
+
+	// takes non-negative integer posts, returns a String representation of a fence with the specified number of posts. Uses a FOR loop.
 	public static String fenceF (int posts){
 		String fence = "";
 		for (int x = 0; x < posts; x++){
@@ -25,8 +29,13 @@ public class Rof {
 		}
 		return fence;
 	}
+
+	// a non-empty String s and returns s with its letters reversed. Uses a FOR loop.
+	public static String reverseF (String s) {
+		String reverse = "";
+		for (int length = s.length(); length > 0; length--) {
+		  reverse += s.substring(length-1, length);
+		}
+		return reverse;
+	  }
 }
-	
-	
-	
-	
