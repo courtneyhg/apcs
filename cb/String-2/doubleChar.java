b60203c47587a5c8c01ad385987b42647178a06c
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-11-01 04:24:19 -0400

    Update doubleChar.java

--- cb/String-2/doubleChar.java
+++ cb/String-2/doubleChar.java
@@ -1,8 +1,8 @@
 public String doubleChar(String str) {
-  String answer "";
-  for (int length = 0; length > str.length(); length --) {
-  	answer += str.substring(0, 1);
-  	answer += str.substring(0, 1);
+  String answer = "";
+  for (int length = 0; length < str.length(); length ++) {
+  	answer += str.substring(length, length + 1);
+    answer += str.substring(length, length + 1);
   }
   return answer;
 }
