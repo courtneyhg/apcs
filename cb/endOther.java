94e8298ac4917f2a6dea8b230332267fd13a82f1
Courtney Huang <90864113+courtneyhg@users.noreply.github.com>
2021-11-01 04:17:52 -0400

    Update endOther.java

---
+++ cb/endOther.java
@@ -0,0 +1,13 @@
+public boolean endOther(String a, String b) {
+  a.toLowerCase() = String a;
+  b.toLowerCase() = String b;
+  int lengthA = a.length(a);
+  int lengthB = b.length(b);
+  String partA = b.substring(lengthB - lengthA, lengthA);
+  if (a.equals.partA) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
