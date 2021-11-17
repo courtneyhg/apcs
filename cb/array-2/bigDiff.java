7a9003cd8a7ef494c11fef77fd3916e98cb8f8ba
courtneyhg <chuang31@stuy.edu>
2021-11-17 05:51:27 -0500

    cb

--- cb/array-2/bigDiff.java
+++ cb/array-2/bigDiff.java
@@ -1,13 +1,13 @@
 public int bigDiff(int[] nums) {
-  int max;
-  int min;
-  for (int i=0; i<nums.length; i++){
-    if (Math.max(nums[i+1], nums[i]) == nums[i]){
+  int max = nums[0];
+  int min = nums[0];
+  for (int i=1; i<nums.length; i++){
+    if (nums[i] > max) {
       max = nums[i];
     }
-      else {
-        min = nums[i];
-      }
+    if (nums[i] < min) {
+      min = nums[i];
     }
-    return max-min;
+  }
+  return max - min;
 }
