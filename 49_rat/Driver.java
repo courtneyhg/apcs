6f4cfb600e35b8e3d239591d26a3c8b2cda2ec18
Courtney Huang <chuang31@stuy.edu>
2021-12-23 13:19:11 -0500

    negative test cases

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -1,13 +1,22 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		Rational z = new Rational(5, 10);
-		Rational x = new Rational(8, 9);
-		String joemama = new String("licky");
+		// Rational z = new Rational(5, 10);
+		// Rational x = new Rational(8, 9);
+		// String joemama = new String("licky");
+		//
+		// System.out.println(z.equals(x));
+		// System.out.println(z.compareTo(x));
+		// System.out.println(z.equals(joemama));
+		// System.out.println(z.compareTo(joemama));
 
-		System.out.println(z.equals(x));
-		System.out.println(z.compareTo(x));
-		System.out.println(z.equals(joemama));
-		System.out.println(z.compareTo(joemama));
+		Rational a = new Rational(3, 7);
+		Rational b = new Rational(8, -5);
+		String bob = new String("no");
+
+		System.out.println(a.equals(b));
+		System.out.println(a.compareTo(b));
+		System.out.println(a.equals(bob));
+		System.out.println(a.compareTo(bob));
 }
 }
