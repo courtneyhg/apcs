d5dca50cccd80fe2dab3ed04dbfc505a0b83520c
courtneyhg <chuang31@stuy.edu>
2022-03-22 06:46:01 -0400

    add act 10

--- cobo/elevens/Activity10  Starter Code/ElevensGUIRunner.java
+++ cobo/elevens/Activity10  Starter Code/ElevensGUIRunner.java
@@ -9,8 +9,14 @@ public class ElevensGUIRunner {
 	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
-		Board board = new ElevensBoard();
+		// Board board = new ElevensBoard();
+		// CardGameGUI gui = new CardGameGUI(board);
+		// gui.displayGame();
+
+		Board board = new ThirteensBoard();
 		CardGameGUI gui = new CardGameGUI(board);
 		gui.displayGame();
+
+
 	}
 }
