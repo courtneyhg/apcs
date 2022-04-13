6e28f87260e666a36bfa91f8364f6102aa8dd27d
courtneyhg <chuang31@stuy.edu>
2022-04-12 21:35:26 -0400

    update

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,23 +1,48 @@
+/*
+Duolingo -- Brianna Tieu, Courtney Huang, and Xinqing Lin
+APCS pd6
+HW90 -- Swabbing the Deque
+2022-04-12
+time spent: 0.5 hr
+*/
+
 public interface Deque<QUASAR> {
-  //means of adding an element to collection:
-  //Enqueue an element onto the back of this queue.
+  //adds an element to collection onto the back of this deque.
   public void addLast(QUASAR x);
 
-  //means of adding an element to collection:
-  //Enqueue an element onto the front of this queue.
+  //adds an element to collection onto the front of this deque.
   public void addFirst(QUASAR x);
 
-  //means of removing an element from collection:
-  //Dequeues and returns the last element of the queue.
+  //removes last element from collection and returns removed element.
   public QUASAR removeLast();
 
-  //means of removing an element from collection:
-  //Dequeues and returns the first element of the queue.
+  //removes first element from collection and returns removed element.
   public QUASAR removeFirst();
 
-  public QUASAR peekFirst();
-
+  //returns last element from collection, returns null if deque is empty.
   public QUASAR peekLast();
 
+  //returns first element from collection, returns null if deque is empty.
+  public QUASAR peekFirst();
+
+  //adds element to back of deque unless capacity restrictions are violated.
+  public boolean offerLast();
+
+  //adds element to front of deque unless capacity restrictions are violated.
+  public boolean offerFirst();
+
+  //returns last element from collection.
+  public QUASAR getLast();
+
+  //returns first element from collection.
+  public QUASAR getFirst();
+
+  //returns and removes last element from collection, returns null if deque is empty.
+  public QUASAR pollLast();
+
+  //returns and removes first element from collection, returns null if deque is empty.
+  public QUASAR pollFirst();
+
+  //returns size of the collection.
   public int size();
 }
