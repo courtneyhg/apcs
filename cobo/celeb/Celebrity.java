e610001bc29cef21998e8cd1a2e52407b12890f7
courtneyhg <chuang31@stuy.edu>
2022-04-26 20:56:32 -0400

    upload

---
+++ cobo/celeb/Celebrity.java
@@ -0,0 +1,80 @@
+// FIRE: Fang Chen, Courtney Huang, Brianna Tieu
+// apcs pd06
+// L09 -- Celeb
+// 2022-04-27
+// time spent: __ hrs
+
+/**
+ * Celebrity base class for the Celebrity game.
+ * @author cody.henrichsen
+ * @version 1.4 17/09/2018
+ */
+public class Celebrity
+{
+	/**
+	 * The clue to determine the celebrity
+	 */
+	 private String _clue;
+
+	/**
+	 * The answer or name of the celebrity.
+	 */
+	 private String _answer;
+
+	/**
+	 * Creates a Celebrity instance with the supplied answer and clue
+	 * @param answer
+	 * @param clue
+	 */
+	public Celebrity(String answer, String clue)
+	{
+		_answer = answer;
+		_clue = clue;
+	}
+
+	/**
+	 * Supplies the clue for the celebrity
+	 * @return
+	 */
+	public String getClue()
+	{
+		return _clue;
+	}
+
+	/**
+	 * Supplies the answer for the celebrity.
+	 * @return
+	 */
+	public String getAnswer()
+	{
+		return _answer;
+	}
+
+	/**
+	 * Updates the clue to the provided String.
+	 * @param clue The new clue.
+	 */
+	public void setClue(String clue)
+	{
+		_clue = clue;
+	}
+
+	/**
+	 * Updates the answer to the provided String.
+	 * @param answer The new answer.
+	 */
+	public void setAnswer(String answer)
+	{
+		_answer = answer;
+	}
+
+	/**
+	 * Provides a String representation of the Celebrity.
+	 */
+	@Override
+	public String toString()
+	{
+		return "The celebrity is " + _answer + " and your clue is " + _clue;
+	}
+
+}
---
+++ cobo/celeb/README.md
@@ -0,0 +1,15 @@
+# TNPG: FIRE
+## Roster: Fang Chen, Courtney Huang, Brianna Tieu
+
+### TO-DO
+- ~~Finish skimming / reading the lab~~
+- ~~Go back and write the Celebrity class~~
+  - Answer guiding qs
+- Write CelebrityGame class
+- Write Celebrity subclass
+- Go back and answer the Check for Understanding questions and other accessory questions
+
+Summary of Experience:
+Activity 1 was pretty fun.
+
+Activity 2
